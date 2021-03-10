18bf472cca4c8ca8abff42077a0de0168db6cedf
techman22 <79210760+techman22@users.noreply.github.com>
2021-03-09 19:32:08 -0600

    Add files via upload

---
+++ Assets/LightSource.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+using UnityEngine.SceneManagement;
+
+public class LightSource : MonoBehaviour
+{
+    GameObject playerFire;
+    Light2D TheLight;
+    //public Transform flamePosition;
+    //public GameObject projectile;
+    //public float projectileSpeed;
+    //private Rigidbody2D rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerFire = GameObject.FindWithTag("Fire");
+        TheLight = playerFire.GetComponent<Light2D>();
+       FindObjectOfType<AudioManager>().Play("Fire");
+        //rigidbody = GetComponent<Rigidbody2D>();
+        //rigidbody.velocity = transform.right * projectileSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+        TheLight.pointLightOuterRadius -= .0009f;
+        if(TheLight.pointLightOuterRadius <= 0f)
+        {
+            Destroy(playerFire);
+            SceneManager.LoadScene("SampleScene");
+        }
+        //if (Input.GetKeyDown(KeyCode.E))
+        //{
+        //    Instantiate(projectile, flamePosition.position, flamePosition.rotation);
+
+        //}
+    }
+
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    // if(collision.tag == "Ground")
+    //    {
+    //        // Instantiate(impactEffect, transform.position, Quaternion.identity);
+    //        // Destroy(collision.gameObject);
+    //        Destroy(gameObject);
+    //    }
+    //}
+}
---
+++ Assets/Movement.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+public class Movement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    SpriteRenderer sprite;
+    public float speed;
+    public float jumpForce;
+    bool isJumping = false;
+    public Transform jumpingChecker;
+    public float checkGroundRadius;
+    public LayerMask groundLayer;
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Jump();
+        CheckIfJumping();
+    }
+
+    void Move()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float moveBy = x * speed;
+        rb.velocity = new Vector2(moveBy, rb.velocity.y);
+
+        if (Input.GetKey/*Down*/(KeyCode.D))
+        {
+            anim.Play("Lost_Walk_Right");
+            sprite.flipX = false;
+            // FindObjectOfType<AudioManager>().Play("Walking");
+
+        }
+        if (Input.GetKey/*Down*/(KeyCode.A))
+        {
+            anim.Play("Lost_Walk_Right");/*Left*/
+            sprite.flipX = true;
+            // FindObjectOfType<AudioManager>().Play("Walking");
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            anim.Play("Lost_Idle");
+            // FindObjectOfType<AudioManager>().Pause("Walking");
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            anim.Play("Lost_Idle");/*_left*/
+            //  FindObjectOfType<AudioManager>().Pause("Walking");
+        }
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !isJumping)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            anim.Play("Lost_Jump");
+        }
+    }
+
+    void CheckIfJumping()
+    {
+        Collider2D collider = Physics2D.OverlapCircle(jumpingChecker.position, checkGroundRadius, groundLayer);
+        if (collider != null)
+        {
+            isJumping = false;
+        }
+        else
+        {
+            isJumping = true;
+        }
+    }
+}
